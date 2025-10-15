c44af890f0450ac892a5e81b1d3cf657e46583ec
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-10-15 14:06:51 +0700

    [Zeta] No longer modify SF
    Signed-off-by: LoggingNewMemory <albert.wesley.dion@gmail.com>

--- Modules/Scripts/Zeta.sh
+++ Modules/Scripts/Zeta.sh
@@ -1,5 +1,3 @@
-service call SurfaceFlinger 1035 i32 0
-
 # Get highest rate By: Koneko_Dev
 max_rate=$(cmd display dump 2>/dev/null | grep -Eo 'fps=[0-9.]+' | cut -f2 -d= | awk '{printf "%.0f\n", $1}' | sort -nr | head -n1)
 
