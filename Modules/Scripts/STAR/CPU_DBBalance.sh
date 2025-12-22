2eb3f0f54a338f7e9dbf7cda8377d54f340f0d71
KanagawaYamada <yamadakernel.project@gmail.com>
2025-12-22 21:26:07 +0700

    Attempt to better execution time
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/Scripts/STAR/CPU_DBBalance.sh
+++ Modules/Scripts/STAR/CPU_DBBalance.sh
@@ -1,19 +1,33 @@
 ###################################
 # Star Tweak (@Koneko_dev)
 # Version amba 2.0 
-# Note: No debug.hwui.target_cpu_time_percent, etc
 ###################################
 
-maincore () {
-atrace --async_stop 1>/dev/null
-cmd looper_stats reset;cmd looper_stats disable
-for app in $(pm list packages | cut -d: -f2); do pm log-visibility --disable $app; done &
-for a in --reset --disable --disable-detailed-tracking;do dumpsys binder_calls_stats $a;done
-for b in --clear --stop-testing; do dumpsys procstats $b;done
-for c in ab-logging-disable dwb-logging-disable dmd-logging-disable; do cmd display $c;done
-for f in $(dumpsys window | grep "^  Proto:" | sed 's/^  Proto: //' | tr ' ' '\n'; dumpsys window | grep "^  Logcat:" | sed 's/^  Logcat: //' | tr ' ' '\n'); do wm logging disable "$f"; wm logging disable-text "$f"; done
+maincore() {
+    atrace --async_stop >/dev/null 2>&1 &
+    cmd looper_stats reset >/dev/null 2>&1 &
+    cmd looper_stats disable >/dev/null 2>&1 &
+
+    pm list packages | cut -d: -f2 | while read app; do
+        pm log-visibility --disable "$app" >/dev/null 2>&1 &
+    done &
+
+    for a in --reset --disable --disable-detailed-tracking; do
+        dumpsys binder_calls_stats $a >/dev/null 2>&1 &
+    done
+
+    for b in --clear --stop-testing; do
+        dumpsys procstats $b >/dev/null 2>&1 &
+    done
+
+    for c in ab-logging-disable dwb-logging-disable dmd-logging-disable; do
+        cmd display $c >/dev/null 2>&1 &
+    done
+
+    dumpsys window | grep -E "^  (Proto|Logcat):" | cut -d: -f2 | tr -d ' ' | while read f; do
+        wm logging disable "$f" >/dev/null 2>&1 &
+        wm logging disable-text "$f" >/dev/null 2>&1 &
+    done &
 }
 
-
-#trigger action
 maincore
--- Modules/Scripts/STAR/CPU_DBPerformance.sh
+++ Modules/Scripts/STAR/CPU_DBPerformance.sh
@@ -3,41 +3,40 @@
 ###################################
 # Star Tweak (@Koneko_dev)
 # Version amba 2.0 
-# Note: Remove set_hwui_sf because crash SF in Arona
 ###################################
 
 maincore() {
-    atrace --async_stop >/dev/null 2>&1 || true
-    cmd looper_stats reset >/dev/null 2>&1 || true
-    cmd looper_stats disable >/dev/null 2>&1 || true
+    atrace --async_stop >/dev/null 2>&1 &
+    cmd looper_stats reset >/dev/null 2>&1 &
+    cmd looper_stats disable >/dev/null 2>&1 &
 
-    for prop in debug.debuggerd.wait_for_debugger debug.debuggerd.wait_for_gdb; do
-        setprop $prop false
-    done
+    setprop debug.debuggerd.wait_for_debugger false
+    setprop debug.debuggerd.wait_for_gdb false
     setprop debug.debuggerd.disable 1
 
     for opt in --reset --disable --disable-detailed-tracking; do
-        dumpsys binder_calls_stats $opt 
+        dumpsys binder_calls_stats $opt >/dev/null 2>&1 &
     done
+
     for opt in --clear --stop-testing; do
-        dumpsys procstats $opt   
+        dumpsys procstats $opt >/dev/null 2>&1 &
     done
 
     for flag in ab-logging-disable dwb-logging-disable dmd-logging-disable; do
-        cmd display $flag 
+        cmd display $flag >/dev/null 2>&1 &
     done
 
-    logcat -G 64K 
-    logcat -c    
+    logcat -G 64K >/dev/null 2>&1
+    logcat -c
 }
 
 toggle_off_powersave() {
-    cmd power set-mode 0 
-    cmd power set-adaptive-power-saver-enabled false 
-    cmd power thermalservice override-status 0 
-    settings delete global battery_saver_constants 
+    cmd power set-mode 0
+    cmd power set-adaptive-power-saver-enabled false
+    cmd power thermalservice override-status 0
+    settings delete global battery_saver_constants
 }
 
 maincore
-toggle_off_powersave 
+toggle_off_powersave
 sync
