7ff055ea3d6e446724490ceb02fc356f99a41575
KanagawaYamada <yamadakernel.project@gmail.com>
2026-01-14 22:30:05 +0700

    Remove set-adaptive-power-saver-enabled to fix Wifi's gone on when powersave
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/Scripts/STAR/BATTERY_RESTORE.sh
+++ Modules/Scripts/STAR/BATTERY_RESTORE.sh
@@ -4,6 +4,5 @@
 ###################################
 
 cmd power set-mode 0
-cmd power set-adaptive-power-saver-enabled false
 cmd power thermalservice override-status 0
 settings delete global battery_saver_constants
