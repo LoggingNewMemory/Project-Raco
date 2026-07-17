bf13e8a8e27acd4678504d0bea3c46efc9e08c2d
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-07-17 11:13:50 +0700

    I have to work for ts
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- AppSource2/app/src/main/java/com/kanagawa/yamada/project/raco/GameAssistantService.kt
+++ AppSource2/app/src/main/java/com/kanagawa/yamada/project/raco/GameAssistantService.kt
@@ -2,11 +2,7 @@ package com.kanagawa.yamada.project.raco
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.AccessibilityServiceInfo
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import android.view.accessibility.AccessibilityEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,26 +19,6 @@ class GameAssistantService : AccessibilityService() {
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC
         info.notificationTimeout = 100
         serviceInfo = info
-        
-        createNotificationChannel()
-        val notification = Notification.Builder(this, "raco_game_assistant")
-            .setContentTitle("Project Raco Game Assistant is Running")
-            .setSmallIcon(android.R.drawable.ic_menu_manage)
-            .build()
-            
-        startForeground(1, notification)
-    }
-    
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "raco_game_assistant",
-                "Game Assistant",
-                NotificationManager.IMPORTANCE_MIN
-            )
-            val manager = getSystemService(NotificationManager::class.java)
-            manager.createNotificationChannel(channel)
-        }
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
