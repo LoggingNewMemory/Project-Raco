12c86331ad3237daccab507683a7dcbd9b59cf91
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-09-18 21:13:19 +0700

    Resolve merge conflicts and fix downscale bugs

--- Sources/raco_main.c
+++ Sources/raco_main.c
@@ -490,11 +490,7 @@ int main(int argc, char *argv[]) {
                 mode_awaken();
             }
             rswap_resume(argv[2]);
-<<<<<<< HEAD
             
-
-=======
->>>>>>> b1d4a280fcedbae72292442dc63d540e74c12a6f
         }
         return 0;
     }
@@ -506,11 +502,7 @@ int main(int argc, char *argv[]) {
         mode_normal();
         if (argc >= 3) {
             rswap_suspend(argv[2]);
-<<<<<<< HEAD
             
-
-=======
->>>>>>> b1d4a280fcedbae72292442dc63d540e74c12a6f
         }
         return 0;
     }
